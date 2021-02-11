bb5cb11be89116c7a292b360a870949ae45c7fba
LauHe <78902080+LauHe@users.noreply.github.com>
2021-02-11 12:08:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://optimistic-wilson-2055b5.netlify.app/'
+        origin: 'https://optimistic-wilson-2055b5.netlify.app'
     })
 );
 app.options('*', cors());
